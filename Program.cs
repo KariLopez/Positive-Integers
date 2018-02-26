eba3142a464879cfc78e386a8cb4cec783aa8e82
klopez91 <31086446+klopez91@users.noreply.github.com>
2018-02-26 13:00:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string i;
+			Console.WriteLine("Please enter a positive integer");
+			i = Console.ReadLine();
+				try
+				{
+					int m = Int32.Parse(i);
+				}
+				catch (FormatException)
+				{
+					Console.WriteLine("Input is not a string of digits");
+				}
+				catch (OverflowException)
+				{
+					Console.WriteLine("Number is too long");
+				}
+				catch (DivideByZeroException)
+				{
+					Console.WriteLine("Value is zero");
+				}
+				finally
+				{
+					Console.ReadLine();
+				}
+		}
+			
+	}
+}
